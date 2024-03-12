1083a4befdc6e8db3dc1cb76e987457340fc737f
Sanjeev Sahoo <sanjeevs.ttl@tatamotors.com>
2024-03-12 15:43:07 +0530

    oracle db connection done

---
+++ src/_common/db/oracledb/connection.ts
@@ -0,0 +1,40 @@
+import oracledb, { BindParameters, ExecuteOptions } from "oracledb";
+
+const DB_CONFIG = {
+  user: process.env.BASE_ORACLEDB_USER,
+  password: process.env.BASE_ORACLEDB_PASSWORD,
+  connectString: process.env.BASE_ORACLEDB_URL,
+};
+
+function simpleQuery(
+  statement: string,
+  binds: BindParameters = [],
+  opts: ExecuteOptions = {}
+) {
+  return new Promise(async (resolve, reject) => {
+    oracledb.initOracleClient();
+    let conn;
+
+    opts.outFormat = oracledb.OUT_FORMAT_OBJECT;
+    opts.autoCommit = true;
+
+    try {
+      conn = await oracledb.getConnection(DB_CONFIG);
+      const result = await conn.execute(statement, binds, opts);
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    } finally {
+      if (conn) {
+        // conn assignment worked, need to close
+        try {
+          await conn.close();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  });
+}
+
+export { simpleQuery };
--- src/app/api/Users/route.ts
+++ src/app/api/Users/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
-import { findUserList } from "@/_common/db/mongodb/services/users";
+import * as MongoDBUsers from "@/_common/db/mongodb/services/users";
+import * as OracleDBUsers from "@/_common/db/oracledb/services/users";
 import { BASE_DB_TYPE } from "@/_common/constants";
 
 export async function GET() {
   try {
-    const users = BASE_DB_TYPE === "mongodb" ? await findUserList() : [];
+    const users =
+      BASE_DB_TYPE === "mongodb"
+        ? await MongoDBUsers.findUserList()
+        : await OracleDBUsers.findUserList();
 
     return NextResponse.json({ users }, { status: 200 });
   } catch (err) {

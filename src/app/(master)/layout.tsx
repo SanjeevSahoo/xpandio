70c9cdd8825187275dd7290c5bc6118de8034bd8
Sanjeev Sahoo <sanjeevs.ttl@tatamotors.com>
2024-03-13 14:12:49 +0530

    extra information passed to auth

--- src/app/(master)/layout.tsx
+++ src/app/(master)/layout.tsx
@@ -9,7 +9,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await auth();
-  console.log(session);
+
   return (
     <div className="grid grid-rows-[auto_1fr] h-screen w-screen p-1">
       <div className="grid grid-cols-[1fr_auto_auto] justify-between items-center p-1">

65517479b6756ee7e7e4c3e4aeea196f22ead7f5
Sanjeev Sahoo <sanjusoft72539@gmail.com>
2024-05-03 06:37:10 +0530

    layout client provider for next now working

--- src/app/(master)/layout.tsx
+++ src/app/(master)/layout.tsx
@@ -1,22 +1,27 @@
 import Sidebar from "./layout/Sidebar";
 import Header from "./layout/Header";
 import Settings from "./layout/Settings";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "../auth";
 
 export default async function MasterLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
-    <div className="grid grid-rows-[1fr] h-screen w-screen bg-background text-foreground relative overflow-hidden">
-      <div className="grid grid-cols-[auto_1fr] w-full h-full overflow-auto ">
-        <Sidebar />
-        <div className="grid grid-rows-[auto_1fr] overflow-auto">
-          <Header />
-          <div className="h-full w-full overflow-auto">{children}</div>
+    <SessionProvider session={session}>
+      <div className="grid grid-rows-[1fr] h-screen w-screen bg-background text-foreground relative overflow-hidden">
+        <div className="grid grid-cols-[auto_1fr] w-full h-full overflow-auto ">
+          <Sidebar />
+          <div className="grid grid-rows-[auto_1fr] overflow-auto">
+            <Header />
+            <div className="h-full w-full overflow-auto">{children}</div>
+          </div>
         </div>
+        <Settings />
       </div>
-      <Settings />
-    </div>
+    </SessionProvider>
   );
 }
